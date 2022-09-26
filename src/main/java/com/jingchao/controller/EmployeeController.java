c273cff570095a604c0b8d3f09801f256abc47f6
JingChao <jc.jingchao@qq.com>
2022-09-26 21:14:38 +0800

    update files

--- src/main/java/com/jingchao/controller/EmployeeController.java
+++ src/main/java/com/jingchao/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.jingchao.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class EmployeeController {
@@ -19,12 +16,18 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @RequestMapping("/employee/page/{pageNum}")
-    public String getEmployeePage(@PathVariable("pageNum") Integer pageNum, Model model){
+    @ResponseBody
+    public PageInfo<Emp> getEmployeePage(@PathVariable("pageNum") Integer pageNum){
+        PageInfo<Emp> pageInfo = employeeService.getEmployeePage(pageNum);
+        // System.out.println(pageInfo);
+        return pageInfo;
+    }
+    /* public String getEmployeePage(@PathVariable("pageNum") Integer pageNum, Model model){
         PageInfo<Emp> page = employeeService.getEmployeePage(pageNum);
         model.addAttribute("page", page);
         System.out.println(page);
         return "employee";
-    }
+    } */
 
     @PostMapping("/employee")
     public String addNewEmployee(@RequestBody Emp emp){
